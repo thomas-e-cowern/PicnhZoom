e708b2477f309276c847aeace7faf80ca1112bca
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-02-28 08:42:56 -0500

    PinchZoom project commit

---
+++ PinchZoom/PinchZoom/PinchZoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchZoomApp.swift
+//  PinchZoom
+//
+//  Created by Thomas Cowern on 2/28/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchZoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
