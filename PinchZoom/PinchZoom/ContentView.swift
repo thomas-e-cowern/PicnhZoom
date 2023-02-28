3e4cf03260f2e312ab036197d994c0f9c98b88ac
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-02-28 09:45:40 -0500

    moved thumbnail images to assets folder

--- PinchZoom/PinchZoom/ContentView.swift
+++ PinchZoom/PinchZoom/ContentView.swift
@@ -10,9 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
+            Image("thumb-magazine-back-cover")
+                .resizable()
+                .scaledToFit()
             Text("Hello, world!")
         }
         .padding()
